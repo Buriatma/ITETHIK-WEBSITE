44149ae41392dce4cecbcb6d230e2f2b0caa81d2
LEGEND X <manjeetsingh7224@gmail.com>
2023-05-12 15:18:30 +0530

    RE STORING

--- admin/config/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('../config/header.php');
-
-if (!isset($_SESSION['user-id'])) {
-    header('location: ' . ROOT_URL . 'signin.php');
-    die();
-}
--- config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-define('ROOT_URL', 'http://localhost/itethik-blog/');
-define('DB_NAME', 'itethik-blog');
-define('DB_PASS', 'legend1234');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'legend');
