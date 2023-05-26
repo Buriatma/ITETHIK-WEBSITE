507101aac45654b0f0d1551a1d3f1427f5aae1a2
HeySkidee <harshitojha46@gmail.com>
2023-05-26 11:26:47 +0530

    parallax done

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,20 @@ const toggleNavbar = function (){
 }
 
 addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
+
+
+// Parallax effect
+
+const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", event => {
+    for (let i = 0, len = parallaxElements.length; i < len; i++) {
+
+        const movementX = (event.clientX / window.innerWidth) * Number(parallaxElements[i].dataset.parallaxSpeed);
+
+        const movementY = (event.clientY / window.innerHeight) * Number(parallaxElements[i].dataset.parallaxSpeed);
+
+        parallaxElements[i].animate({
+        transform: `translate(${movementX}px, ${movementY}px)`},{duration: 500, fill: "forwards"});
+    }
+})
